3385f9fe17fe57574a7c080e87d49730f8ad9281
yfx <1793185558@qq.com>
2023-08-21 17:21:34 +0800

    standaloneStore engine done

--- kv/storage/standalone_storage/standalone_storage.go
+++ kv/storage/standalone_storage/standalone_storage.go
@@ -6,42 +6,68 @@ import (
 	"github.com/pingcap-incubator/tinykv/kv/storage"
 	"github.com/pingcap-incubator/tinykv/kv/util/engine_util"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
+	"os"
+	"path/filepath"
+	"sync"
 )
 
 // StandAloneStorage is an implementation of `Storage` for a single-node TinyKV instance. It does not
 // communicate with other nodes and all data is stored locally.
 type StandAloneStorage struct {
 	// Your Data Here (1).
-	Engine      *badger.DB
-	AloneReader *StandAloneReader
+	engines *engine_util.Engines
+	config  *config.Config
+	reader  *StandAloneReader
+	wg      *sync.WaitGroup
 }
 
 type StandAloneReader struct {
-	Iter *engine_util.BadgerIterator
+	txn *badger.Txn
+	db  *badger.DB
 }
 
 func NewStandAloneStorage(conf *config.Config) *StandAloneStorage {
 	// Your Code Here (1).
-	return &StandAloneStorage{
-		Engine:      engine_util.CreateDB(conf.DBPath, conf.Raft),
-		AloneReader: new(StandAloneReader),
-	}
+	var sa = &StandAloneStorage{}
+	dbPath := conf.DBPath
+	kvPath := filepath.Join(dbPath, "kv")
+
+	os.MkdirAll(kvPath, os.ModePerm)
+
+	kvDB := engine_util.CreateDB(kvPath, false)
+	sa.engines = engine_util.NewEngines(kvDB, nil, kvPath, "")
+	sa.config = conf
+	sa.wg = new(sync.WaitGroup)
+	sa.reader = new(StandAloneReader)
+	sa.reader.db = kvDB
+
+	return sa
 }
 
 func (s *StandAloneStorage) Start() error {
 	// Your Code Here (1).
+	s.wg.Add(1)
 
+	go func() {
+		s.wg.Wait()
+	}()
 	return nil
 }
 
 func (s *StandAloneStorage) Stop() error {
 	// Your Code Here (1).
-	return s.Engine.Close()
+	defer s.wg.Done()
+	s.reader.Close()
+	err := s.engines.Close()
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (s *StandAloneStorage) Reader(ctx *kvrpcpb.Context) (storage.StorageReader, error) {
 	// Your Code Here (1).
-	return s.AloneReader, nil
+	return s.reader, nil
 }
 
 func (s *StandAloneStorage) Write(ctx *kvrpcpb.Context, batch []storage.Modify) error {
@@ -49,34 +75,34 @@ func (s *StandAloneStorage) Write(ctx *kvrpcpb.Context, batch []storage.Modify) 
 	for _, modify := range batch {
 		switch modify.Data.(type) {
 		case storage.Put:
-			s.Engine.Update(func(txn *badger.Txn) error {
-				if err := txn.Set(modify.Key(), modify.Value()); err != nil {
-					return err
-				}
-				return nil
-			})
+			err := engine_util.PutCF(s.engines.Kv, modify.Cf(), modify.Key(), modify.Value())
+			return err
 		case storage.Delete:
-			s.Engine.Update(func(txn *badger.Txn) error {
-				if err := txn.Delete(modify.Key()); err != nil {
-					return err
-				}
-				return nil
-			})
+			err := engine_util.DeleteCF(s.engines.Kv, modify.Cf(), modify.Key())
+			return err
 		}
 	}
 	return nil
 }
 
 func (r *StandAloneReader) GetCF(cf string, key []byte) ([]byte, error) {
+	val, err := engine_util.GetCF(r.db, cf, key)
+	if err != nil {
+		if err.Error() == "Key not found" {
+			return nil, nil
+		} else {
+			return nil, err
+		}
 
-	return nil, nil
+	}
+	return val, nil
 }
 
 func (r *StandAloneReader) IterCF(cf string) engine_util.DBIterator {
-	txn := &badger.Txn{}
-	return engine_util.NewCFIterator(cf, txn)
+	r.txn = r.db.NewTransaction(false)
+	return engine_util.NewCFIterator(cf, r.txn)
 }
 
 func (r *StandAloneReader) Close() {
-	r.Iter.Close()
+
 }
