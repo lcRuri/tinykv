8ed3946f3d16d0337c14376447d53ccffcb80b0b
yfx <1793185558@qq.com>
2023-08-21 09:25:14 +0800

    project1 update

--- kv/storage/standalone_storage/standalone_storage.go
+++ kv/storage/standalone_storage/standalone_storage.go
@@ -12,19 +12,19 @@ import (
 // communicate with other nodes and all data is stored locally.
 type StandAloneStorage struct {
 	// Your Data Here (1).
-	engine engine_util.Engines
+	Engine      *badger.DB
+	AloneReader *StandAloneReader
 }
 
-type StandAloneStorageReader struct {
+type StandAloneReader struct {
+	Iter *engine_util.BadgerIterator
 }
 
 func NewStandAloneStorage(conf *config.Config) *StandAloneStorage {
 	// Your Code Here (1).
 	return &StandAloneStorage{
-		engine: engine_util.Engines{
-			Kv:     new(badger.DB),
-			KvPath: conf.DBPath,
-		},
+		Engine:      engine_util.CreateDB(conf.DBPath, conf.Raft),
+		AloneReader: new(StandAloneReader),
 	}
 }
 
@@ -36,13 +36,12 @@ func (s *StandAloneStorage) Start() error {
 
 func (s *StandAloneStorage) Stop() error {
 	// Your Code Here (1).
-	return s.engine.Close()
+	return s.Engine.Close()
 }
 
 func (s *StandAloneStorage) Reader(ctx *kvrpcpb.Context) (storage.StorageReader, error) {
 	// Your Code Here (1).
-
-	return nil, nil
+	return s.AloneReader, nil
 }
 
 func (s *StandAloneStorage) Write(ctx *kvrpcpb.Context, batch []storage.Modify) error {
@@ -50,14 +49,14 @@ func (s *StandAloneStorage) Write(ctx *kvrpcpb.Context, batch []storage.Modify) 
 	for _, modify := range batch {
 		switch modify.Data.(type) {
 		case storage.Put:
-			s.engine.Kv.Update(func(txn *badger.Txn) error {
+			s.Engine.Update(func(txn *badger.Txn) error {
 				if err := txn.Set(modify.Key(), modify.Value()); err != nil {
 					return err
 				}
 				return nil
 			})
 		case storage.Delete:
-			s.engine.Kv.Update(func(txn *badger.Txn) error {
+			s.Engine.Update(func(txn *badger.Txn) error {
 				if err := txn.Delete(modify.Key()); err != nil {
 					return err
 				}
@@ -68,15 +67,16 @@ func (s *StandAloneStorage) Write(ctx *kvrpcpb.Context, batch []storage.Modify) 
 	return nil
 }
 
-func (s *StandAloneStorageReader) GetCF(cf string, key []byte) ([]byte, error) {
+func (r *StandAloneReader) GetCF(cf string, key []byte) ([]byte, error) {
 
 	return nil, nil
 }
 
-func (s *StandAloneStorageReader) IterCF(cf string) engine_util.DBIterator {
-	return nil
+func (r *StandAloneReader) IterCF(cf string) engine_util.DBIterator {
+	txn := &badger.Txn{}
+	return engine_util.NewCFIterator(cf, txn)
 }
 
-func (s *StandAloneStorageReader) Close() {
-
+func (r *StandAloneReader) Close() {
+	r.Iter.Close()
 }
