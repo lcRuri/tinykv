522bf7bebc16f2fd3d60985e34c540e4046bfa2d
yfx <1793185558@qq.com>
2024-02-29 15:50:59 +0800

    feat: lab4c scanner

--- kv/transaction/mvcc/scanner.go
+++ kv/transaction/mvcc/scanner.go
@@ -1,6 +1,8 @@
 package mvcc
 
 import (
+	"bytes"
+	"encoding/binary"
 	"github.com/pingcap-incubator/tinykv/kv/util/engine_util"
 )
 
@@ -32,16 +34,55 @@ func (scan *Scanner) Close() {
 // Next returns the next key/value pair from the scanner. If the scanner is exhausted, then it will return `nil, nil, nil`.
 func (scan *Scanner) Next() ([]byte, []byte, error) {
 	// Your Code Here (4C).
-	scan.iter.Next()
 	if !scan.iter.Valid() {
 		return nil, nil, nil
 	}
-	key := scan.iter.Item().Key()
-	value, err := scan.iter.Item().Value()
+	key := scan.iter.Item().KeyCopy(nil)
+	userKey := DecodeUserKey(key)
+
+	//底层存储key是按照key的顺序来排的
+	//这就可能导致当前key的ts超过txn的ts 但是之后的key的ts可能是满足条件的
+	//于是需要我们按照当前txn的ts seek一下
+	scan.iter.Seek(EncodeKey(userKey, scan.txn.StartTS))
+	key1 := scan.iter.Item().KeyCopy(nil)
+	decodeUserKey := DecodeUserKey(key1)
+	//比较了不同之后 那么我们就需要继续递归 找到满足条件的
+	if !bytes.Equal(decodeUserKey, userKey) {
+		return scan.Next()
+	}
+
+	//这样得到的value才是满足ts的
+	value, err := scan.iter.Item().ValueCopy(nil)
 	if err != nil {
+		return userKey, nil, err
+	}
+
+	//key被删除了
+	if WriteKind(value[0]) == WriteKindDelete {
+		return nil, nil, err
+	}
+	var ts uint64
+	if value != nil {
+		ts = binary.BigEndian.Uint64(value[1:])
+	} else {
 		return nil, nil, err
 	}
 
-	return key, value, err
+	val, err := scan.txn.Reader.GetCF(engine_util.CfDefault, EncodeKey(userKey, ts))
+
+	for {
+		scan.iter.Next()
+		if !scan.iter.Valid() {
+			break
+		}
+		tmpKey := scan.iter.Item().KeyCopy(nil)
+		tmpUserkey := DecodeUserKey(tmpKey)
+		if !bytes.Equal(tmpUserkey, userKey) {
+			break
+		}
+
+	}
+
+	return userKey, val, err
 
 }
